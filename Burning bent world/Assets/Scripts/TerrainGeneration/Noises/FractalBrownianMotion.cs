116a1343abe68f70f3c0bdb13776aeb2ba17fd55
wollfie <leowolllff@gmail.com>
2023-04-27 16:09:28 +0200

    Base of code

---
+++ Burning bent world/Assets/Scripts/TerrainGeneration/Noises/FractalBrownianMotion.cs
@@ -0,0 +1,7 @@
+﻿namespace TerrainGeneration.Noises
+{
+    public class FractalBrownianMotion
+    {
+        
+    }
+}
---
+++ Burning bent world/Assets/Scripts/TerrainGeneration/TerrainGenerator.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TerrainGeneration
+{
+    public static class TerrainGenerator
+    {
+        
+    }
+
+}
