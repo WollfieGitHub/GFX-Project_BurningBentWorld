c5d448f25f7d9c12ef61bd2f6dc7cf2f8f9f8f7d
Dylan <dylan.vairoli@gmail.com>
2023-05-09 11:39:10 +0200

    Add FirePlacer

--- Burning bent world/Assets/FirePlacer.cs
+++ Burning bent world/Assets/FirePlacer.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 
 public class FirePlacer : MonoBehaviour
 {
-    /*[SerializeField]
-    private GameObject*/
+    [SerializeField] private GameObject firePrefab;
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -16,8 +16,13 @@ public class FirePlacer : MonoBehaviour
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out var hit, Single.PositiveInfinity))
         {
-            
-            
+            var pos = hit.point;
+            var roundedPos = new Vector3(
+                Mathf.RoundToInt(pos.x),
+                Mathf.RoundToInt(pos.y),
+                Mathf.RoundToInt(pos.z)
+            );
+            Instantiate(firePrefab, roundedPos, Quaternion.identity);
         }
     }
 }
